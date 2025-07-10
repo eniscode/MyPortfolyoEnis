e816d74d6a010c5f09518f0a17c6f1b80308f559
Enis <2203022020@student.beykent.edu.tr>
2025-07-10 14:26:13 +0300

    Proje dosyası ekle.

---
+++ MyPortfolyoEnis/Controllers/DefaultController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolyoEnis.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        
+        public IActionResult Skills()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult References()
+        {
+            return View();
+        }
+
+        public IActionResult Certificates()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+    }
+}
